3b632f690f2cdfebad2bb3a1bcba599449a31f57
Bishal Moktan <bishalmoktan270@gmail.com>
2024-12-01 18:13:53 +0530

    feat: complete alll sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Features from "./components/features";
+import Footer from "./components/footer";
 import Hero from "./components/hero";
 import Highlights from "./components/highlights";
+import HowItWorks from "./components/how-it-works";
 import Model from "./components/model";
 import Navbar from "./components/navbar";
 import * as Sentry from "@sentry/react";
@@ -13,6 +15,8 @@ function App() {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 }
